fed0c4d9be98048a6daa5fec18303f5297a92d5f
fabiochidda <fabio.chidda99@gmail.com>
2022-02-24 02:11:18 +0100

    Aggiunti titolo ed immagine

---
+++ app.js
@@ -0,0 +1,9 @@
+const title = new Vue({
+    el: "#title_box",
+    data: {
+        titolo: "Titolo random per un'immagine random",
+        img: "https://picsum.photos/200"
+    }
+});
+
+console.log(title)
